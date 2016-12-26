d91048ee63dba8dfb9e9ed0da91841f639a43c5b
wahyu pratama <wahyulucky28@gmail.com>
2016-12-27 00:25:15 +0700

    tambah google analitics

--- another.php
+++ another.php
@@ -199,55 +199,47 @@
     </section>
 
     <section id="galery">
-        <div class="col-md-12 text-center">
-            <h1>Gallery</h1>
+        <div id="galery-id" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#galery-id" data-slide-to="0" class=""></li>
+            <li data-target="#galery-id" data-slide-to="1" class=""></li>
+            <li data-target="#galery-id" data-slide-to="2" class="active"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="item">
+              <img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide" src="http://4k-background.com/highresolution/l_265.jpg" width="900px" height="600px">
+              <div class="container">
+                <div class="carousel-caption">
+                  <h1>Example headline.</h1>
+                  <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+                  <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+                </div>
+              </div>
+            </div>
+            <div class="item">
+              <img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" alt="Second slide" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzY2NiI+PC9yZWN0Pjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojNmE2YTZhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjU2cHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+U2Vjb25kIHNsaWRlPC90ZXh0Pjwvc3ZnPg==">
+              <div class="container">
+                <div class="carousel-caption">
+                  <h1>Another example headline.</h1>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                  <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+                </div>
+              </div>
+            </div>
+            <div class="item active">
+              <img data-src="holder.js/900x500/auto/#555:#5a5a5a/text:Third slide" alt="Third slide" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzU1NSI+PC9yZWN0Pjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojNWE1YTVhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjU2cHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+VGhpcmQgc2xpZGU8L3RleHQ+PC9zdmc+">
+              <div class="container">
+                <div class="carousel-caption">
+                  <h1>One more for good measure.</h1>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                  <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <a class="left carousel-control" href="#galery-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+          <a class="right carousel-control" href="#galery-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
         </div>
-        <div class="col-md-12">
-            <ul class="sec-image">
-                <li>
-                    <figure class="gal-photo">
-                        <img src="img/kegiatan1o.jpg" class="img img-responsive" alt="">
-                    </figure>
-                </li>
-                <li>
-                    <figure class="gal-photo">
-                        <img src="img/kegiatan2o.jpg" class="img img-responsive" alt="">
-                    </figure>
-                </li>
-                <li>
-                    <figure class="gal-photo">
-                        <img src="img/kegiatan3o.jpg" class="img img-responsive" alt="">
-                    </figure>
-                </li>
-                <li>
-                    <figure class="gal-photo">
-                        <img src="img/workshop1o.jpg" class="img img-responsive" alt="">
-                    </figure>
-                </li>
-                <li>
-                    <figure class="gal-photo">
-                        <img src="img/workshop2o.jpg" class="img img-responsive" alt="">
-                    </figure>
-                </li>
-                <li>
-                    <figure class="gal-photo">
-                        <img src="img/workshop3o.jpg" class="img img-responsive" alt="">
-                    </figure>
-                </li>
-                <li>
-                    <figure class="gal-photo">
-                        <img src="img/kegiatan1o.jpg" class="img img-responsive" alt="">
-                    </figure>
-                </li>
-                <li>
-                    <figure class="gal-photo">
-                        <img src="img/workshop3o.jpg" class="img img-responsive" alt="">
-                    </figure>
-                </li>
-            </ul>
-            <div class="add"></div>
-        </div>
-        <div class="add"></div>
     </section>
 
     <footer>
@@ -307,7 +299,16 @@
         </footer>
 
 
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+      ga('create', 'UA-89458197-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
 
     <script type="text/javascript" src="js/jquery.js"></script>
     <script src="js/waypoints.min.js"></script>
