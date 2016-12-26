93567c0e8adf219196056255cd05af4110b9a444
wahyu pratama <wahyulucky28@gmail.com>
2016-12-26 12:52:43 +0700

    repair desain

--- index.php
+++ index.php
@@ -17,26 +17,30 @@
     <header>
         <ul class="navigation">
           <li><a class="brand" href="#">UKM IT</a></li>
-          <li><a href="#news">News</a></li>
-          <li><a href="#contact">Contact</a></li>
-          <li class="right"><a href="#about">About</a></li>
-          <li><a href="#news">Home</a></li>
+          <li><a href="#">Sign Up</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Gallery</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Profil</a></li>
+          <li><a href="#">Home</a></li>
         </ul>
-        <ul class="mini-navigation">
+        <!-- <ul class="mini-navigation">
           <li><a class="brand" href="#">UKM IT</a></li>
           <li><a href="#news">News</a></li>
           <li><a href="#contact">Contact</a></li>
           <li class="right"><a href="#about">About</a></li>
           <li><a href="#news">Home</a></li>
-        </ul>
+        </ul> -->
 
         <div id="mySidenav" class="sidenav">
           <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
           <a href="#">Home</a>
+          <a href="#">Profil</a>
           <a href="#">About</a>
-          <a href="#">Services</a>
-          <a href="#">Clients</a>
+          <a href="#">Gallery</a>
           <a href="#">Contact</a>
+          <a href="#">Login</a>
+          <a href="#">Sign Up</a>
         </div>
         <span id="smallnav" style="font-size:30px;cursor:pointer;" onclick="openNav()">&#9776;</span>
         <div class="container">
@@ -44,8 +48,8 @@
                 <div class="col-md-10 col-md-offset-1 text-center head" style="margin-top: 100px" >
                     <h1>UKM IT - STIKOMCKI</h1>
                     <p>#Salam Koding Otak Pusing Jari Keriting</p>
-                    <a href="#" class="btn btn=primary btnloghead btn-lg ">Login</a>
-                    <a href="#" class="btn btn-warning btnlearn btn-lg js--section-to-feature">Learn More</a>
+                    <a href="#" class="btn btn=primary btnloghead btn-lg hidden-sm hidden-xs">Login</a>
+                    <a href="#" class="btn btn-warning btnlearn btn-lg js--section-to-feature hidden-sm hidden-xs">Learn More</a>
                 </div>
 
             </div>
@@ -54,7 +58,29 @@
 
     <div class="clearfix"></div>
 
-    <section id="welcome">
+    <section id="detail">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="text-center">Apa itu UKM IT ?</h1>
+                </div>
+                <div class="add"></div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="img/LAMBANG_UKM.png" class="img img-rensponsive js-wp-5" width="50%" alt="">
+                </div>
+                <div class="col-md-6">
+                    <p>
+                        UKM IT adalah salah satu UKM dari 7 UKM yg berada di kampus STIKOM CKI Buaran. Dengan adanya UKM ini, diharapkan dapat membantu para mahasiswa STIKOM CKI buaran dalam hal pemecahan masalah yg dihadapi yg berhubungan dengan Informasi Teknologi serta menjadi wadah bagi para mahasiswa untuk menyalurkan bakat dalam bidang komputer sehingga menghasilkan tenaga-tenaga kerja yg professional dalam bidangnya.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="add"></div>
+    </section>
+
+    <!-- <section id="welcome">
         <div class="container">
             <div class="row">
                 <div class="col-md-8 col-md-offset-2 ">
@@ -65,7 +91,7 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> -->
 
     <section id="feature" class="js--section-feature">
         <div class="container">
@@ -82,18 +108,22 @@
                     <div class="col-md-4">
                         <i class="glyphicon glyphicon-user icon-big"></i>
                         <h2>Kegiatan Mingguan</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolores ducimus enim illo, ex amet sed voluptatem </p>
+                        <p>
+                           Membahas dan mempelajari ilmu yang bercakupan degan dunia IT, lebih difokuskan untuk mempelajari programming desktop/mobile, web, desain dan networking
+                        </p>
                     </div>
                     <div class="col-md-4 js-wp-3">
                         <i class="glyphicon glyphicon-plus icon-big"></i>
-                        <h2>Kegiatan Mingguan</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolores ducimus enim illo, ex amet sed voluptatem
+                        <h2>Majalah UKM</h2>
+                        <p>
+                            Diakhir bulannya UKM IT membuat sebuah majalah kampus agar mahasiswa bisa mengetahui kegiatan apa saja yang berada dalam ukm it
                         </p>
                     </div>
                     <div class="col-md-4 js-wp-4">
                         <i class="glyphicon glyphicon-plus icon-big"></i>
-                        <h2>Kegiatan Mingguan</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolores ducimus enim illo, ex amet sed voluptatem
+                        <h2>Seminar IT</h2>
+                        <p>
+                            Kegiatan ini digunakan kepada para mahasiswa STIKOM CKI untuk menambahkan pengetahuan tentang ilmu yang mencakup komputer diluar jam perkuliahan
                         </p>
                     </div>
                 </div>
@@ -153,32 +183,12 @@
         <div class="add"></div>
     </section>
 
-    <section id="detail">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h1 class="text-center">Apa itu UKM IT ?</h1>
-                </div>
-                <div class="add"></div>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <img src="img/LAMBANG_UKM.png" class="img img-rensponsive js-wp-5" width="50%" alt="">
-                </div>
-                <div class="col-md-6">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus tempore iste nisi provident obcaecati explicabo illo reiciendis recusandae laboriosam voluptatum eveniet ipsum, alias adipisci commodi rerum optio fugit molestias esse consequatur nam accusantium reprehenderit culpa cumque earum. Dolores molestiae, ad vitae fugit modi molestias sequi alias officia architecto voluptatibus autem animi distinctio corporis iure minima vel
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div class="add"></div>
-    </section>
+
 
     <section id="portofolio">
         <div class="container">
             <div class="row">
-                <div class="col-md-10 col-md-offset-1">
+                <div class="col-md-10 col-md-offset-1 port-in">
                     <div class="col-md-12">
                         <h1>Hasil kerjaan kita</h1>
                         <div class="add"></div>
@@ -187,18 +197,75 @@
                     <div class="col-md-6">
                         <a href="#"><img src="img/port1.jpg" alt="" class="img img-responsive" width="100%"></a>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-6 ketiga">
                         <div class="col-md-6"><img src="img/port1.jpg" alt="" class="img img-responsive" width="100%"></div>
                         <div class="col-md-6"><img src="img/port1.jpg" alt="" class="img img-responsive" width="100%"></div>
-                        <div class="add"></div><br>
+                        <div class="clearfix visible-lg-block down"></div>
                         <div class="col-md-6"><img src="img/port1.jpg" alt="" class="img img-responsive" width="100%"></div>
                         <div class="col-md-6"><img src="img/port1.jpg" alt="" class="img img-responsive" width="100%"></div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="add">
     </section>
+
+
+    <footer>
+            <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-5">
+                    <!-- <a href="" class="footer-brand"><img src="/images/ugm_footer.png" alt="Universitas Gadjah Mada"></a> -->
+                    <address style="padding-top: 15px; color: #fff">
+                        <img src="http://localhost/ci_web//assets/img/LAMBANG_UKM_BW.png" class="img img-responsive" width="40%"><div style="margin-bottom: 10px"></div>
+                        <strong style="color: #fff">Unit Kegiatan Mahasiswa IT</strong><div style="margin-bottom: 10px; color: #fff;"></div>
+                        Jl Radin Inten II, Duren Sawit Jakarta Timur<div style="margin-bottom: 10px"></div>
+                        <i class="fa fa-envelope"></i>&nbsp;&nbsp;
+                        <a href="#">stikomcki@stikomcki.ac.id</a><div style="margin-bottom: 10px"></div>
+                        <i class="fa fa-phone"></i>&nbsp;&nbsp;
+                        <a href="tel:+62-274-588688">+62 (274) 588688</a><div style="margin-bottom: 10px"></div>
+                        <!-- F: <a href="tel:+62-274-565223">+62(274)565223</a> -->
+                    </address>
+                </div>
+                <div class="col-md-9 col-sm-7" >
+                    <div class="row">
+                        <div class="col-md-3 col-sm-6 col-md-offset-6">
+                        <div style="color: #fff;">
+                            <h3>Tentang UKM</h3>
+                        </div>
+                        <div>
+                            <ul id="Menu">
+                                <li>
+                                    <a href="http://localhost/ci_web/tentang.html">Sejarah</a></li>
+                                <li>
+                                    <a href="http://localhost/ci_web/tentang.html">Struktur Organisasi</a></li>
+                                <li>
+                                    <a href="http://localhost/ci_web/profil.html">Visi dan Misi</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div style="color: #fff;">
+                            <h3>Kerja sama</h3>
+                        </div>
+                        <div>
+                            <ul id="Menu">
+                                <li>
+                                    <a href="http://dka.ugm.ac.id/">Kerja sama Dalam Negeri</a>
+                                </li>
+                                <li>
+                                    <a href="http://alumni.ugm.ac.id/v3.0/">Alumni</a>
+                                </li>
+                                <li>
+                                    <a href="http://oia.ugm.ac.id">Urusan Internasional</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </footer>
+
 
 
 
